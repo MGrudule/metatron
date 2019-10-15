635fedbe44b9cc8173483bd7faf20d78f0de5a92
Maija <hello@maijagrudule.com>
2019-10-15 22:35:37 +0200

    Added points for shapes

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ let Pattern = {
     "5,13", //5 topLeft
     //inner
     "19,13", //6 innerTop
-    '26,17', //7 innerTopRight
-    '26,25', //8 innerBottomRight
+    "26,17", //7 innerTopRight
+    "26,25", //8 innerBottomRight
     "19,29", //9 innerBottom
-    '12,25', //10 innerBottomLeft
-    '12,17', //11 innerTopLeft
+    "12,25", //10 innerBottomLeft
+    "12,17", //11 innerTopLeft
     //center
     "19, 21" //12 center
   ],
@@ -20,127 +20,120 @@ let Pattern = {
   plot: function(points) {
     let shape = [];
     for (const point of points) {
-      shape.push(Pattern.coordinates[point])
+      shape.push(Pattern.coordinates[point]);
     }
-    return shape.join('L')
+    return shape.join("L");
   },
   draw: function(plot, closed) {
     let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    let closure = closed ? "z" : '';
+    let closure = closed ? "z" : "";
     path.setAttribute("d", "M" + plot + closure);
-    path.classList.add('path-hex-new');
+    path.classList.add("animated");
 
     return path;
   }
 };
 
 window.addEventListener("DOMContentLoaded", function(event) {
-
+  let svg = document.getElementById("svg");
+  let center = document.getElementById("c12");
+  center.addEventListener("mouseover", () => addCircle(), false);
 });
 
-function addShape() {
+let lines = {
+  c0: {
+    shape: [0, 1, 2, 3, 4, 5],
+    closed: true
+  },
+  c1: {
+    shape: [1, 3, 5],
+    closed: true
+  },
+  c2: {
+    shape: [4, 0, 2],
+    closed: true
+  },
+  c3: {
+    shape: [3, 0],
+    closed: true
+  },
+  c4: {
+    shape: [4, 1],
+    closed: true
+  },
+  c5: {
+    shape: [5, 2],
+    closed: true
+  },
+  c6: {
+    shape: [6, 7, 8, 9, 10, 11],
+    closed: true
+  },
+  c7: {
+    shape: [7, 5, 7, 3],
+    closed: true
+  },
+  c8: {
+    shape: [8, 6, 10],
+    closed: true
+  },
+  c9: {
+    shape: [5, 2],
+    closed: true
+  },
+  c10: {
+    shape: [10, 0, 2],
+    closed: false
+  },
+  c11: {
+    shape: [11, 3, 11, 1],
+    closed: false
+  }
+};
 
-  let svg = document.getElementById("svg")
-  let path = Pattern.draw(Pattern.plot([0, 1, 2, 3, 4, 5]), true);
+for (let [line, value] of Object.entries(lines)) {
+  let circle = document.getElementById(line);
+  let path = Pattern.draw(Pattern.plot(value.shape), value.closed);
   svg.appendChild(path);
+  setTimeout(function() {
+    path.remove();
+  }, 5000);
+  circle.addEventListener("mouseover", () => addShape(value), false);
 }
 
-function addShape2() {
-
-  let svg = document.getElementById("svg")
-  let path = Pattern.draw(Pattern.plot([1, 3, 5]), true);
-  svg.appendChild(path);
+function addShape(value) {
+  let newPath = Pattern.draw(Pattern.plot(value.shape), value.closed);
+  svg.appendChild(newPath);
+  setTimeout(function() {
+    newPath.remove();
+  }, 5000);
 }
 
-function addShape3() {
-
-  let svg = document.getElementById("svg")
-  let path = Pattern.draw(Pattern.plot([4, 0, 2]), true);
-  svg.appendChild(path);
-}
-
-function addShape4() {
-
-  let svg = document.getElementById("svg")
-  let path = Pattern.draw(Pattern.plot([3, 0]), true);
-  svg.appendChild(path);
-}
-
-function addShape5() {
-
-  let svg = document.getElementById("svg")
-  let path = Pattern.draw(Pattern.plot([4, 1]), true);
-  svg.appendChild(path);
-}
-
-function addShape6() {
-
-  let svg = document.getElementById("svg")
-  let path = Pattern.draw(Pattern.plot([5, 2]), true);
-  svg.appendChild(path);
-}
-
-function addShape7() {
-  console.log('hmms')
-  let svg = document.getElementById("svg")
-  let path = Pattern.draw(Pattern.plot([6, 7, 8, 9, 10, 11]), true);
-  svg.appendChild(path);
-}
-
-function addShape8() {
-
-  let svg = document.getElementById("svg")
-  let path = Pattern.draw(Pattern.plot([7, 5, 7, 3]), true);
-  svg.appendChild(path);
-}
-
-function addShape9() {
-
-  let svg = document.getElementById("svg")
-  let path = Pattern.draw(Pattern.plot([8, 6, 10]), true);
-  svg.appendChild(path);
-}
-
-function addShape10() {
-
-  let svg = document.getElementById("svg")
-  let path = Pattern.draw(Pattern.plot([9, 11, 7]), true);
-  svg.appendChild(path);
-}
-
-function addShape11() {
-
-  let svg = document.getElementById("svg")
-  let path = Pattern.draw(Pattern.plot([10, 0, 2]), false);
-  svg.appendChild(path);
-}
-
-function addShape12() {
-
-  let svg = document.getElementById("svg")
-  let path = Pattern.draw(Pattern.plot([11, 3, 11, 1]), false);
-  svg.appendChild(path);
-}
-
-function addShape13() {
-
-  let svg = document.getElementById("svg")
+function addCircle() {
   let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-  let circle2 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-  circle.setAttribute("r", 17)
-  circle.setAttribute("cx", 19)
+  let circle2 = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "circle"
+  );
+  circle.setAttribute("r", 15.7);
+  circle.setAttribute("cx", 19);
   circle.setAttribute("cy", 21);
-  circle.classList.add('path-hex-circle');
-  svg.appendChild(circle);
-  circle2.setAttribute("r", 19)
-  circle2.setAttribute("cx", 19)
+  circle.classList.add("path-circle");
+  circle2.setAttribute("r", 16.5);
+  circle2.setAttribute("cx", 19);
   circle2.setAttribute("cy", 21);
-  circle2.classList.add('path-hex-circle');
-
-
+  circle2.classList.add("path-circle");
+  circle2.classList.add("sm");
+  svg.appendChild(circle);
 
   setTimeout(function() {
     svg.appendChild(circle2);
-  }, 1000);
+  }, 500);
 
+  setTimeout(function() {
+    circle.remove();
+  }, 5000);
+  setTimeout(function() {
+    circle2.remove();
+  }, 5500);
 }
